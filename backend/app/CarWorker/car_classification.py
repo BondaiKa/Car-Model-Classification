7053d22511fda658364d4f5ea1bffe4cfdf1869e
Karim <st.herbinar@gmail.com>
2020-11-24 16:08:36 +0300

    add simple backend

--- backend/app/CarWorker/car_classification.py
+++ backend/app/CarWorker/car_classification.py
@@ -1,15 +1,39 @@
 import tensorflow as tf
+import logging
+import numpy as np
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
-model = tf.keras.models.load_model('model/final-vgg-model')
+
+model = tf.keras.models.load_model('final-vgg-model')
+
+CAR_CLASSES = [
+    'KAMAZ_ALLKAMAZ_C',
+    'LADA_PRIORA_B',
+    'MAZDA_3_B',
+    'RЕNАULТ_DUSТЕR_B',
+    'SCANIA_ALLSCANIA_C',
+    'TOYOTA_RАV4_B',
+    'VOLVO_ALLVOLVO_C',
+    'VОLКSWАGЕN_TIGUAN_B',
+    'VОLКSWАGЕN_РОLО_B',
+    'КIА_RIО_B',
+    'НУUNDАI_SОLАRIS_B'
+]
 
 
 def image_preprocessing(image):
-    img_arr = tf.keras.preprocessing.image.img_to_array(image)
-    img_arr = tf.expand_dims(img_arr, 0)
+    logger.info('Decode image...')
+    img_arr = tf.image.resize(tf.io.decode_image(image,channels=3), [256, 256])
+    img_arr = tf.expand_dims(img_arr, axis=0)
+    logger.info(img_arr.shape)
     return img_arr
-    
+
 
 def recognise_photo(image):
     image = image_preprocessing(image)
-    res = model.predict(image)
-    return res
+    logger.info('Start to predict image...')
+    res = model.predict(image, batch_size=1).flatten()
+    num_class = np.argmax(res)
+    name_class = CAR_CLASSES[num_class]
+    return (res,name_class)
--- backend/app/CarWorker/main.py
+++ backend/app/CarWorker/main.py
@@ -1,17 +1,49 @@
+import logging
 from typing import Optional
-from fastapi import FastAPI, File, UploadFile
-from fastapi import FastAPI, Request
+
+from fastapi import FastAPI, File, Request, UploadFile
+from fastapi.openapi.utils import get_openapi
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 from car_classification import recognise_photo
 
+logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def photo_upload(photo):
-    res = recognise_photo(photo)
-    return templates.TemplateResponse("index.html", {"res": res})
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Car classification API",
+        version="1.0",
+        description="This is a very simple car model classificatipon API schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+@app.post("/")
+async def photo_upload(my_file: UploadFile = File(...)):
+    """
+    send image to recongise and return classification results
+    """
+    logger.info("Loading and classification image")
+    with open(my_file.filename, "wb") as f:
+        image = await my_file.read()
+    
+    res, name_class =  recognise_photo(image)
+    return {"values": str(res),"class":name_class}
+
+
+app.openapi = custom_openapi
